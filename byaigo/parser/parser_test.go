008fe0227d2683f74615f6c59a511ad7445d192b
Brice V <bricevadnais@yahoo.com>
2019-05-29 20:26:06 -0400

    can now collect errors from execution

--- byaigo/parser/parser_test.go
+++ byaigo/parser/parser_test.go
@@ -37,6 +37,21 @@ let foobar = 5353;
 			return
 		}
 	}
+
+	checkParserErrors(t, p)
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
