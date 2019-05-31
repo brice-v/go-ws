133d408dfbca12c72d2b2479407371588aa92bf0
Brice V <bricevadnais@yahoo.com>
2019-05-30 21:00:19 -0400

    we can now get integer values

--- byaigo/ast/ast.go
+++ byaigo/ast/ast.go
@@ -101,6 +101,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
 func (p *Program) String() string {
 	var out bytes.Buffer
 
