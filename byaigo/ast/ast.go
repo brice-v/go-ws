67e3f5921152e2cb3cf82ecd4e8848c5fe043b9c
Brice V <bricevadnais@yahoo.com>
2019-06-01 21:06:03 -0400

    we now have prefix operators (! and -)

--- byaigo/ast/ast.go
+++ byaigo/ast/ast.go
@@ -45,6 +45,24 @@ type Identifier struct {
 	Value string
 }
 
+type PrefixExpression struct {
+	Token    token.Token // The prefix token ie. !, -
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 func (es *ExpressionStatement) String() string {
