21b9473a0a16c864fd8d7290016513ba85d7f092
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-13 17:14:54 -0400

    fixed integers printout

--- byaigo/object/object.go
+++ byaigo/object/object.go
@@ -19,7 +19,7 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i) }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
