927b559a80b6bfd9995558db34940c66d54f4939
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-16 16:37:42 -0400

    added another test and case for let statement

--- byaigo/evaluator/evaluator.go
+++ byaigo/evaluator/evaluator.go
@@ -28,6 +28,12 @@ func Eval(node ast.Node) object.Object {
 			return val
 		}
 		return &object.ReturnValue{Value: val}
+	case *ast.LetStatement:
+		val := Eval(node.Value)
+		if isError(val) {
+			return val
+		}
+		// TODO HANDLE THIS
 
 	// Expressions
 	case *ast.IntegerLiteral:
