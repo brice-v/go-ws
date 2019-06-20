0f081263c9258b5c866bd7c0b7ed237c587902ff
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-19 20:01:05 -0400

    we now have builtin len function

--- byaigo/object/object.go
+++ byaigo/object/object.go
@@ -35,6 +35,9 @@ const (
 
 	// FUNCTION_OBJ is the function object type
 	FUNCTION_OBJ = "FUNCTION"
+
+	// BUILTIN_OBJ is the builtin function object type
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 type Function struct {
@@ -131,3 +134,12 @@ func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 
 // Inspect returns a stringified version of the object for debugging
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
