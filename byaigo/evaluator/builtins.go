56ba6b559033c3eb87a07141649e26b3766b3d4c
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-19 21:34:12 -0400

    added puts - Printed `Hello World!` !!!!!!!!

--- byaigo/evaluator/builtins.go
+++ byaigo/evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"byaigo/object"
+	"fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -84,6 +85,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
