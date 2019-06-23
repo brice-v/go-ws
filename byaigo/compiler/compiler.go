b71596b35768849c37de640790ae2f5717884bc0
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-23 10:56:32 -0400

    compiler is now able to emit opadd

--- byaigo/compiler/compiler.go
+++ byaigo/compiler/compiler.go
@@ -4,6 +4,7 @@ import (
 	"byaigo/ast"
 	"byaigo/code"
 	"byaigo/object"
+	"fmt"
 )
 
 type Compiler struct {
@@ -41,6 +42,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
