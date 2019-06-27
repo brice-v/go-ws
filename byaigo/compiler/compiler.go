4d9cc4381388497372ac6b272458c066c1ddeec5
Brice Vadnais <bricevadnais@yahoo.com>
2019-06-27 18:15:59 -0400

    function literal is now compiled by vm

--- byaigo/compiler/compiler.go
+++ byaigo/compiler/compiler.go
@@ -221,7 +221,25 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpIndex)
+	case *ast.FunctionLiteral:
+		c.enterScope()
 
+		err := c.Compile(node.Body)
+		if err != nil {
+			return err
+		}
+
+		instructions := c.leaveScope()
+
+		compileFn := &object.CompiledFunction{Instructions: instructions}
+		c.emit(code.OpConstant, c.addConstant(compileFn))
+	case *ast.ReturnStatement:
+		err := c.Compile(node.ReturnValue)
+		if err != nil {
+			return err
+		}
+
+		c.emit(code.OpReturnValue)
 	}
 	return nil
 }
