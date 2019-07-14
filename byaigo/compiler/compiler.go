0cc7bf0b1d330071fd16014a829f48c16611045b
Brice Vadnais <bricevadnais@yahoo.com>
2019-07-14 16:09:14 -0400

    successfully able to run code with global and local definitions

--- byaigo/compiler/compiler.go
+++ byaigo/compiler/compiler.go
@@ -245,9 +245,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpReturn)
 		}
 
+		numLocals := c.symbolTable.numDefinitions
 		instructions := c.leaveScope()
 
-		compileFn := &object.CompiledFunction{Instructions: instructions}
+		compileFn := &object.CompiledFunction{
+			Instructions: instructions,
+			NumLocals:    numLocals,
+		}
 		c.emit(code.OpConstant, c.addConstant(compileFn))
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
