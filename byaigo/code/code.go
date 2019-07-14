675a8d87d7abbfe204e0318198ed22c85660ab5b
Brice Vadnais <bricevadnais@yahoo.com>
2019-07-14 15:48:12 -0400

    properly compiling and resolving global and local scopes

--- byaigo/code/code.go
+++ byaigo/code/code.go
@@ -34,6 +34,9 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+
+	OpGetLocal
+	OpSetLocal
 )
 
 type Definition struct {
@@ -66,6 +69,9 @@ var definitions = map[Opcode]*Definition{
 	OpCall:         {"OpCall", []int{}},
 	OpReturnValue:  {"OpReturnValue", []int{}},
 	OpReturn:       {"OpReturn", []int{}},
+
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -97,12 +103,16 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
 
 	return instruction
 }
+
+func ReadUint8(ins Instructions) uint8 { return uint8(ins[0]) }
 
 func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 	operands := make([]int, len(def.OperandWidths))
@@ -112,6 +122,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 
 		offset += width
